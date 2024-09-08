aa2afac491b746f1b5bb3e0705c6aed8669857c2
amir taherkhani <amirtaherkhani@outlook.com>
2024-09-08 22:50:17 +0330

    Update init.sh
    fix permission error for logs dir

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 ## run file bash init.sh
 mkdir -p ./extensions ./.config  ./vsix ./logs
 sudo chown $USER:$USER ./extensions ./.config  ./vsix ./logs
+sudo chown -R 1000:1000 ./logs  # Ensure that UID 1000 (coder) owns the directory
+sudo chmod -R 755 ./logs  # Grant read, write, and execute permissions
