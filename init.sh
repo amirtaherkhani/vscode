7f29796f8f05c53079e520f50ae78d3118452590
amir taherkhani <amirtaherkhani@outlook.com>
2024-09-09 16:40:20 +0330

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 ## run file bash init.sh
-mkdir -p ./extensions ./.config  ./vsix ./logs
-sudo chown $USER:$USER ./extensions ./.config  ./vsix ./logs
-sudo chown -R 1000:1000 ./logs  # Ensure that UID 1000 (coder) owns the directory
-sudo chmod -R 755 ./logs  # Grant read, write, and execute permissions
+sudo chown -R $USER:$USER . 
+mkdir -p data
+sudo chown -R $USER:$USER data
+sudo chmod -R 755 data  # Grant read, write, and execute permissions
